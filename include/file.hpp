a22b4af66f4c489c0624c850fed2aee5f06a1866
Raposão <danilo-raposo@hotmail.com>
2021-09-08 11:11:43 -0300

    read and write ranking implemented

--- include/file.hpp
+++ include/file.hpp
@@ -5,14 +5,21 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 #include <stdlib.h>
 #include <time.h>
 
+using namespace std;
+
 // Função que dado um arquivo com nome file e uma palavra adiciona essa palavra dentro do arquivo
-void writeWord(std::string word, const char * file);
+void writeWord(string word, const char * file);
+// Função que escreve o ranking no arquivo de ranking
+void writeRanking(const char * file, vector<pair <string, string> > ranking);
 // Função que retorna todos os nomes armazenados no arquivo passado como parametro
-std::vector<std::string> allWords(const char * file);
+vector<string> allWords(const char * file);
 // Função que dado vector de strings retorna uma dessas strings aleatoriamente
-std::string randomWord(std::vector<std::string> allWords);
+string randomWord(vector<string> allWords);
+// Função que pega o nome do arquivo de ranking e retorna um vector de pair com os rankings
+vector< pair<string, string> > readRanking(const char * file);
 
 #endif
--- src/file.cpp
+++ src/file.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-
-
 // Função que dado um arquivo com nome file e uma palavra adiciona essa palavra dentro do arquivo
 void writeWord(string word, const char * file){
     ofstream fileWrite;
@@ -46,4 +44,48 @@ string randomWord(vector<string> allWords){
     int index = rand() % allWords.size();
 
     return allWords[index];
+}
+// Função que pega o nome do arquivo de ranking e retorna um vector de pair com os rankings
+vector< pair<string, string> > readRanking(const char * file){
+    ifstream fileRead;
+
+    fileRead.open(file);
+
+    vector< pair<string,string> > vpair;
+    string sname, snumber;
+    char c;
+
+    while(fileRead.get(c)){
+        if(c == '\n'){
+            for(int i = sname.size() - 1; sname[i] != ' '; i--){
+                snumber.push_back(sname[i]);
+                sname.erase(i);
+            }
+
+            sname.erase(sname.size() - 1);// retirar o ultimo espaço em branco
+            reverse(snumber.begin(), snumber.end());
+
+            vpair.push_back(make_pair(sname, snumber));
+            sname.clear();
+            snumber.clear();
+        }
+        else{
+            sname.push_back(c);
+        }
+    }
+    fileRead.close();
+
+    return vpair;
+}
+// Função que escreve o ranking no arquivo de ranking
+void writeRanking(const char * file, vector<pair <string, string> > ranking){
+    ofstream fileWrite;
+
+    fileWrite.open(file);
+
+    for(int i = 0; i < ranking.size(); i++){
+        fileWrite << ranking[i].first << " " << ranking[i].second << "\n";
+    }
+
+    fileWrite.close();
 }
